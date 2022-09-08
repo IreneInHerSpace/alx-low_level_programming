5a6e985ac8440268477f89b2744e4d4de512ba93
Irene Siteketa <insiteketa@gmail.com>
2022-09-08 12:45:38 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main function is the entry point
- *
- * Return 0
- */
+*main function is the entry point
+* Return: 0
+*/
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
+{
+  puts("\"Programming is like building a multilingual puzzle");
   return (0);
-  
 }
