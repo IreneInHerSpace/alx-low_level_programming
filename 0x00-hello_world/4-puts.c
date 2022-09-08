0ff4329bb5f4a0d2b4a535416ad7bf44bb3cca7f
Irene Siteketa <insiteketa@gmail.com>
2022-09-08 00:57:50 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(void)
+  
+{
+  
+  char test_str[] = "\"Programming is like building a multilingual puzzle";
+  
+
+  
+  puts(test_str);
+  
+  return (0);
+  
+}
