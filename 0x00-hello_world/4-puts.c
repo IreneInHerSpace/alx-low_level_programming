1fd28d66a73e7fc8f18773bc6da4193e993590fa
Irene Siteketa <insiteketa@gmail.com>
2022-09-08 01:24:12 -0700

    task 14

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /** The program is executed in the main function
- *
- *the array test_string stores "Programming is like building a multilingual puzzle" to be passed to the puts function for printing
- *
- *The returned value is a 0
+*the array test_string stores "Programming is like building a multilingual puzzle" to be passed to the puts function for printing
+*The returned value is a 0
 */
 
 int main(void)
 {
-    char test_str[] = "\"Programming is like building a multilingual puzzle";
+
+  char test_str[] = "\"Programming is like building a multilingual puzzle";
+
   puts(test_str);
   return (0); 
+
 }
