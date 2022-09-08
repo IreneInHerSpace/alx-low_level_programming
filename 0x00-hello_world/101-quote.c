178a80372ae93c66fa5c7dfe43b113d951614292
Irene Siteketa <insiteketa@gmail.com>
2022-09-08 12:18:56 -0700

    task 8 remove space

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-  int s;
+int s;
 
-  s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-  write(2,
+write(2,
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 	s);
-  return (1);
+return (1);
 }
