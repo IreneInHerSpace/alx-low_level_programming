ededbd3cd58f08a479e338c6d95f0316adade3a8
Irene Siteketa <insiteketa@gmail.com>
2022-09-09 14:31:19 -0700

    task 10

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,22 +19,17 @@
 int main(void)
 {
 int c;
-int d;
-int e = 0;
-while (e < 10)
-{
-d = 0;
+int d = 0;
 while (d < 10)
 {
 c = 0;
 while (c < 10)
 {
-if (c != d && d != e && e < d && d < c)
+  if (d != c && d < c)
 {
-putchar('0' + e);
 putchar('0' + d);
 putchar('0' + c);
-if (c + d + e != 9 + 8 + 7)
+if (c + d != 17)
 {
 putchar(',');
 putchar(' ');
