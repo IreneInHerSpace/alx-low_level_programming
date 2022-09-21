d1014d18aea1f863a3cd9d76cf7ab862c5e8b4ef
Irene Siteketa <insiteketa@gmail.com>
2022-09-21 11:46:38 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,17 +7,16 @@
 */
 char *_strcat(char *dest, char *src)
 {
-int i=0;
-int j=0;
-while (dest[i] != '\0')
+int i = 0;
+int j;
+while (dest[i])
 {
 i++;
 }
-while (src[j] != '\0')
+for (j = 0; src[j] != '\0'; j++)
 {
 dest[i] = src[j];
 j++;
-i++;
 }
 dest[i] = '\0';
 return (dest);
