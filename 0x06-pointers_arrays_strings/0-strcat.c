bc7a8f27499a26037c6a3c140bceedee98c2ec3b
Irene Siteketa <insiteketa@gmail.com>
2022-09-21 10:30:57 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
 * *_strcat - function commute srtings
-* @dest: param pointer to a char
-* @src: param pointer to a char
+* @dest: string to append to
+* @src: string to add
 * Return: return value of dest
 */
 char *_strcat(char *dest, char *src)
 {
-int i;
-int j;
-i = 0;
-j = 0;
+int i=0;
+int j=0;
 while (dest[i] != '\0')
 {
 i++;
