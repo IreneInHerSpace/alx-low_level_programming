5641d73dfbb6831361a9405166caec6de76856bc
Irene Siteketa <insiteketa@gmail.com>
2022-10-03 06:22:27 -0700

    0-isupper.c rewrite

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -3,9 +3,11 @@
  *_isupper - function that verifies if a  character is uppercase
  *@c: tested character
  *
- *Return: 0 always
+ *Return: returns 1 if it is uppercase , 0 if not
  */
 int _isupper(int c)
 {
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
 return (0);
 }
