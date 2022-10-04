066addf05487350711afd135b53278402660ccab
IreneInHerSpace <insiteketa@gmail.com>
2022-10-04 03:39:36 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,9 @@
  *@argv: Array name
  *Return: 0 always (sucess)
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
-
+	if (argc > 0)
+		printf("%s\n", argv[0]);
 	return (0);
 }
