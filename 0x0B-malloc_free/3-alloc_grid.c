7ab9863230b4caa262720d2139f3df1c7a700f19
Irene Siteketa <insiteketa@gmail.com>
2022-10-03 17:30:32 -0700

    a function that returns a pointer to a 2 dimensional array of integers.

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,6 +1,5 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 /**
  *alloc_grid - Entry point
  *@width: rows of matrix
@@ -10,31 +9,35 @@
 int **alloc_grid(int width, int height)
 {
 int **matrix;
-int i, j, l, *p;
-if (width <= 0 || height <= 0)
+int i, j;
+if (height <= 0 || width <= 0)
+{
 return (NULL);
-matrix = (int **)malloc(height * sizeof(int *));
+}
+matrix = (int **) malloc(sizeof(int *) * height);
 if (matrix == NULL)
+{
 return (NULL);
-for (i = 0; i < height; i++)
-{
-*(matrix + i) = (int *)malloc(width * sizeof(int));
-if (*(matrix + i) == NULL)
-{
-for (i = 0; i < height; i++)
-{
-p = matrix[i];
-free(p);
 }
+for (i = 0; i < height; i++)
+{
+matrix[i] = (int *) malloc(sizeof(int) * width);
+if (matrix[i] == NULL)
+{
 free(matrix);
+for (j = 0; j <= i; j++)
+{
+free(matrix[j]);
+}
 return (NULL);
 }
 }
-for (l = 0; l < height; l++)
+for (i = 0; i < height; i++)
 {
 for (j = 0; j < width; j++)
 {
-matrix[l][j] = 0;
+matrix[i][j] = 0;
+}
 }
 return (matrix);
 }
