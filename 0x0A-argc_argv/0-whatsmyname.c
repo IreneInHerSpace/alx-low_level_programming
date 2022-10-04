79eab30fda47a13ffd1b302fc00afd25973f0666
IreneInHerSpace <insiteketa@gmail.com>
2022-10-04 03:38:36 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
  *main - Prints the name of the program
  *@argc: Number of command line arguments
  *@argv: Array name
- *Return: 0
+ *Return: 0 always (sucess)
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
