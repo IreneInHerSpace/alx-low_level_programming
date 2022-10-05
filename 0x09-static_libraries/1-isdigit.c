aa8663ce827b103944ad080c52f7ea1a66c33983
IreneInHerSpace <insiteketa@gmail.com>
2022-10-05 20:04:37 +0200

    Update 1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,10 +1,19 @@
 #include "main.h"
+
 /**
- *_isdigit - function that verifies if a  character is a digit or not
- *@c: tested character
- *Return: 0
- */
+ * _isdigit - function with int type input
+ * @c: int type input of a single digit
+ * Description:fuction checks for a digit (0 through 9)
+ * Return: 1 if c is digit and 0 other
+*/
 int _isdigit(int c)
 {
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
 return (0);
+}
 }
