15bdac4cfe91059a597d0b003f7537bbea57575a
IreneInHerSpace <insiteketa@gmail.com>
2022-10-05 20:07:38 +0200

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,13 +1,26 @@
 #include "main.h"
 /**
- *strncat - concatenate two strings
- *@dest: string to be appended upon
- *@src: string to be completed at end of dest
- *@n:integer parameter to compare index to
- *
- *Return: 0 always
- */
-char *_strncpy(char *dest, char *src, int n)
+* _strncat - concatenates two strings
+* @dest: input parameter string
+* @src: input parameter string
+* @n: times to iterations
+* Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
 {
-return (0);
+int a;
+int b;
+a = 0;
+while (dest[a] != 0)
+{
+a++;
+}
+b = 0;
+while (src[b] != 0 && b < n)
+{
+dest[a] = src[b];
+a++;
+b++;
+}
+return (dest);
 }
