2a9f93fcf6d7ffa1958ed27c76290457e2a008c6
IreneInHerSpace <insiteketa@gmail.com>
2022-10-05 20:03:48 +0200

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,13 +1,19 @@
 #include "main.h"
 /**
- *_isupper - function that verifies if a  character is uppercase
- *@c: tested character
- *
- *Return: returns 1 if it is uppercase , 0 if not
+ * _isupper - function with 1 int type argument
+ * checks for uppercase character
+ * @c: int type argument with char input
+ * Description: If/else to check if input is lower or uppercase
+ * Return: 1 for uppercase and 0 for other
  */
 int _isupper(int c)
 {
-if ((c >= 'A') && (c <= 'Z'))
+if (c >= 'A' && c <= 'Z')
+{
 return (1);
+}
+else
+{
 return (0);
+}
 }
